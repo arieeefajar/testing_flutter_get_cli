8c185d5d83e6c8fb8822cfb77db45bfa6c529dee
arieeefajar <ariefajar739@gmail.com>
2025-06-24 14:36:28 +0700

    feat(home): func get connect

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -4,18 +4,37 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../controllers/home_controller.dart';
+import '../../../data/models/user_model.dart';
 
 class HomeView extends GetView<HomeController> {
-  const HomeView({super.key});
+  const HomeView({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('HomeView'), centerTitle: true),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () => Get.toNamed(Routes.PRODUCT),
-          child: const Text('Go to product page'),
-        ),
+      body: FutureBuilder<List<User>>(
+        future: controller.getAllUsers(),
+        builder: (context, snap) {
+          if (snap.connectionState == ConnectionState.waiting) {
+            return Center(child: CircularProgressIndicator());
+          }
+          if (snap.data?.length == 0) {
+            return Center(child: Text('No users found'));
+          }
+          return ListView.builder(
+            itemCount: snap.data!.length,
+            itemBuilder: (context, index) {
+              User user = snap.data![index];
+              return ListTile(
+                leading: CircleAvatar(
+                  backgroundImage: NetworkImage(user.avatar ?? ''),
+                ),
+                title: Text('${user.firstName} ${user.lastName}'),
+                subtitle: Text(user.email ?? ''),
+              );
+            },
+          );
+        },
       ),
     );
   }
